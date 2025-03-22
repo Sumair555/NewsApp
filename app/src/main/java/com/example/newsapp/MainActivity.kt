825ef222825bdd238c24bd4f6bd611bee0b46232
sksumair <mohammedsaifuddinsumair_shaik@srmap.edu.in>
2025-03-22 09:02:23 +0530

    internet  permission access given.

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.gfg
+package com.example.newsapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.gfg.ui.theme.NewsAppTheme
+import com.example.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello sumair $name!",
         modifier = modifier
     )
 }
--- app/src/main/java/com/example/newsapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/newsapp/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.gfg.ui.theme
+package com.example.newsapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
