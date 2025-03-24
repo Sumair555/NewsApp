88797fa4a88c2f6bd3dc12bd4c9a8329abd1d58e
sksumair <mohammedsaifuddinsumair_shaik@srmap.edu.in>
2025-03-24 13:45:35 +0530

    chnages going on.

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    kotlin("android") version "1.9.0" apply false
+    kotlin("android.extensions") version "1.9.0" apply false
+    id("com.android.application") version "8.9.0" apply false
+    kotlin("jvm") version "1.8.0"
+}
+
