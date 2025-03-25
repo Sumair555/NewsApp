d9dd47bbe78295f0589e31ca9708fbf792b8a6ca
sksumair <mohammedsaifuddinsumair_shaik@srmap.edu.in>
2025-03-25 13:19:29 +0530

    changes going on

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("android") version "1.9.0" apply false
     kotlin("android.extensions") version "1.9.0" apply false
-    id("com.android.application") version "8.9.0" apply false
+    id("com.android.application") version "8.3.0" apply false  // Update to the latest stable AGP version
     kotlin("jvm") version "1.8.0"
 }
-
